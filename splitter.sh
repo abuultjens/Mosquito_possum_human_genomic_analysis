03d7f85c0d2af179ac46aa38a8fd67cf1385e4a4
Andrew Buultjens <buultjensa@gmail.com>
2023-08-30 23:09:09 +1000

    Update splitter.sh

--- splitter.sh
+++ splitter.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#fofn-checker
-
-#FOFN=36-clinical_fofn.txt
-#OHE_FILE=36-VIC_5-seqcap_full_subset.OHE_v3.csv
-#OUTFILE=36-clinical_36-VIC_5-seqcap_full_subset.OHE_v3.csv
-
 FOFN=${1}
 OHE_FILE=${2}
 OUTFILE=${3}
